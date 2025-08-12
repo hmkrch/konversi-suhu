2a9fa6524edb11344a58690b5f9913eea96b3ed2
Hamka Rachman <hamkaet@gmail.com>
2025-08-12 20:24:09 +0900

    update fitur

--- app.js
+++ app.js
@@ -1,20 +1,53 @@
 const celciusValue = document.getElementById("celciusInpt");
+const fahrenheitValue = document.getElementById("fahrenheitInpt");
 
 const hasilBtn = document.getElementById("cekHasilBtn");
 
 const tampilkanHasil = document.getElementById("tampilkanHasil");
 
-const konversiSuhu = () => {
-  const celcius = Number(celciusValue.value);
+const pilihSuhu = document.getElementById("pilihSuhu");
 
-  return (celcius * 9) / 5 + 32;
+const kolomSuhuF = document.getElementById("kolomF");
+const kolomSuhuC = document.getElementById("kolomC");
+
+pilihSuhu.onchange = () => {
+  if (pilihSuhu.value === "fahrenheit") {
+    kolomSuhuF.classList.remove("hide");
+    kolomSuhuC.classList.add("hide");
+  } else {
+    kolomSuhuF.classList.add("hide");
+    kolomSuhuC.classList.remove("hide");
+  }
 };
+
+// Function
+
+const konversiSuhuCelcius = () => {
+  const c = Number(celciusValue.value);
+
+  return (c * 9) / 5 + 32;
+};
+
+const konversiSuhuFahrenheit = () => {
+  const f = Number(fahrenheitValue.value);
+  const hasil = ((f - 32) * 5) / 9;
+
+  return hasil;
+};
+
+// console.log(konversiSuhuFahrenheit());
 
 // 2 versi
 
-// hasilBtn.onclick = () => {
-//   tampilkanHasil.textContent = konversiSuhu();
-// };
+hasilBtn.onclick = () => {
+  if (!kolomSuhuC.classList.contains("hide")) {
+    tampilkanHasil.textContent = konversiSuhuCelcius();
+  } else {
+    tampilkanHasil.textContent = konversiSuhuFahrenheit();
+  }
+};
+
+// versi tanpa button
 
 // celciusValue.onkeyup = () => {
 //   tampilkanHasil.textContent = konversiSuhu();
